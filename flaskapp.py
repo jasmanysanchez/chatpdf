5a22a26cca52dc8c0f8c1a10c3b8ea12f9bc88b6
Jasmany Sánchez <jasmany.sanchez@unae.edu.ec>
2025-01-08 12:30:59 -0500

    FLASK

---
+++ flaskapp.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+import os
+import json
+from chatpdf import get_pdf_text, get_text_chunks, get_vectorstore, get_conversation_chain, handle_userinput
+from pathlib import Path
+from datetime import datetime
+
+BASE_DIR = Path(__file__).resolve().parent
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/chatpdf", methods=['POST'])
+def chatpdf():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No se envió ningún archivo'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No se seleccionó ningún archivo'}), 400
+    filename = os.path.join(BASE_DIR, app.config['UPLOAD_FOLDER'], f'{datetime.now().strftime("%d%m%Y%H%M%S%f")}_{file.filename}')
+    file.save(filename)
+    file.close()
+    prompt = request.form['prompt']
+    raw_text = get_pdf_text([filename])
+    text_chunks = get_text_chunks(raw_text)
+    vectorstore = get_vectorstore(text_chunks)
+    conversation = get_conversation_chain(vectorstore)
+    response = handle_userinput(prompt, conversation)
+    answer = response.get('answer')
+    if isinstance(answer, str):
+        answer = answer.replace('\n', '')
+        if answer.startswith('```json'):
+            answer = answer.replace('```json', '', 1)
+            answer = answer.replace('```', '', 1)
+        try:
+            d = json.loads(answer)
+            os.path.exists(filename) and os.remove(filename)
+            return jsonify(d)
+        except Exception as ex:
+            pass
+        return 0
+    if isinstance(answer, list) or isinstance(answer, tuple):
+        for i, x in enumerate(answer or []):
+            text = x['text']
+            text = text.replace('\n', '')
+            if text.startswith('```json'):
+                text = text.replace('```json', '', 1)
+                text = text.replace('```', '', 1)
+            try:
+                d = json.loads(text)
+                os.path.exists(filename) and os.remove(filename)
+                return jsonify(d)
+            except Exception as ex:
+                continue
+    return jsonify({'error': 'No se pudo leer el pdf'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
